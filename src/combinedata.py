fdf1269e7fdb43f130f36f38e05d83b19492cd21
KayneRyan <88462495+kayneryan@users.noreply.github.com>
2022-02-15 23:04:20 -0500

    attempt to combine datasets

---
+++ src/combinedata.py
@@ -0,0 +1,37 @@
+import pandas as pd
+
+counties = pd.read_csv('05-01-2021.csv')
+may = pd.read_csv(data/'05-01-2021.csv')
+jun = pd.read_csv(data/'06-01-2021.csv')
+jul = pd.read_csv(data/'07-01-2021.csv')
+aug = pd.read_csv(data/'08-01-2021.csv')
+sep = pd.read_csv(data/'09-01-2021.csv')
+oct = pd.read_csv(data/'10-01-2021.csv')
+nov = pd.read_csv(data/'11-01-2021.csv')
+dec = pd.read_csv(data/'12-01-2021.csv')
+
+countylist = []
+countylist.append(counties.pop('FIPS'))
+
+# unsuccessful attempts to remove blanks and just leave rows with FIPS code
+# for i in range (len(countylist)):    
+#   try:
+#     int(countylist[i])
+#   except:
+#     countylist.pop[i]
+####
+# for i in range (len(countylist)):
+#  a = countylist[i]
+#  if a > 0:
+#    continue
+#  else:
+#    countylist.pop[i]
+#
+#
+
+
+
+fips = {'FIPS': countylist}
+
+
+print(may)
