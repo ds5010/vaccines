d4a2635fbadd0b935e6610edb10a152bb18f20e2
b-mohler <98131490+b-mohler@users.noreply.github.com>
2022-02-15 22:21:21 -0500

    Update merge_data_drop_columns.py
    added comments on running code

--- merge_data_drop_columns.py
+++ merge_data_drop_columns.py
@@ -1,6 +1,8 @@
 #Bridget Mohler
 #15 February 2022
 #This program will combine the JHU and CDC data and remove excess columns (the excess rows are a work in progress on my local repo). 
+#To run the code just python3 merge_data_drop_columns.py
+#If you want to check that it worked you can uncomment the print statements to compare the number of columns in the two files, or uncomment the final statement to actually get the CSV file. 
 import pandas as pd
 
 keep = ["FIPS", "Date", "Series_Complete_Yes", "Deaths"]
@@ -16,3 +18,4 @@ for label in merged_file:
 
 four_column_file = merged_file.drop(drop_list, axis=1)
 #print(len(four_column_file.columns))
+#four_column_file.to_csv("merged_and_cleaned_data.csv")
