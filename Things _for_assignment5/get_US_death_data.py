71f6952f83eebe3ec1d1c5737fce3e9a0b34b53c
Zheng Yune <13390592073@163.com>
2022-02-19 21:53:10 -0500

    retreive US data

---
+++ Things _for_assignment5/get_US_death_data.py
@@ -0,0 +1,15 @@
+# This is how I got all the US death data from 05/01/2021-11/30/2021.
+# I use some regular expression here,which is really powerful
+import glob
+a = glob.glob('../../dead_data/[0][5-9]*-*-2021.csv')
+b = glob.glob('../../dead_data/[1][1]*-*-2021.csv')
+may_to_novenmber = a+b
+may_to_novenmber = sorted(may_to_novenmber)
+final_data = pd.concat(
+    map(pd.read_csv, may_to_novenmber,), ignore_index=True)
+# 删选掉FIPS ==nan的，这个时候 final_data.FIPS.dtype== float64
+final_data = final_data[final_data['FIPS'].isna() == False]
+final_data
+final_data['FIPS'] = final_data['FIPS'].astype('int')
+# final_data.FIPS.dtype
+final_data.to_csv('final_data.csv', index=False)
