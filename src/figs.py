c84e3af6e8913ec5f4c59136590122fa128ad992
Philip <philip.eng.mathieu@gmail.com>
2022-02-22 17:22:39 -0500

    Reorganized code for generating figures

--- src/figs.py
+++ src/figs.py
@@ -1,13 +1,20 @@
-from plot import scatter
-from plotgeo import choropleth
+from plot import *
+from plotgeo import *
 import pandas as pd
 
-
-df = pd.read_csv('../data/merge.csv', converters={"FIPS":str})
-scatter(df, outfile="../figs/All FIPS Scatter Plot.png")
-scatter(df, filters={"CDC_Region":"Region 1"}, outfile="../figs/CDC Region 1 Scatter Plot.png")
-scatter(df, filters={"CDC_Region":"Region 5"}, outfile="../figs/CDC Region 5 Scatter Plot.png")
+# start with merged data and compute additional stats
+df = pd.read_csv('data/merge.csv', converters={"FIPS":str})
 df = compute_stats(df)
-df = regress_multiple(df)
-choropleth(df, outfile="../figs/Choropleth.png")
 
+# create two sample files, one with and one without filtering
+scatter(df, outfile="figs/All FIPS Scatter Plot.png")
+scatter(df, filters={"CDC_Region":"Region 1"}, outfile="figs/CDC Region 1 Scatter Plot.png")
+
+# run regressions (default to state-level)
+df = regress_multiple(df)
+choropleth(df, outfile="figs/Regression by Recip_State Choropleth.png")
+scatter(df, filters={"Recip_State": "FL"}, outfile="figs/Florida Scatter Plot.png")
+
+# run another regression (this time by CDC Region)
+df = regress_multiple(df, column="CDC_Region")
+choropleth(df, column="CDC_Region", outfile="figs/Regression by CDC_Region Choropleth.png")
