25b58454f6663f202cecde7a22ef171e9f4e0b62
Zheng Yune <13390592073@163.com>
2022-02-28 16:31:14 -0500

    add three funciton

---
+++ src/merge_v2.py
@@ -0,0 +1,52 @@
+import pandas as pd
+import numpy as np
+
+"""This function return a dataframe that contains merge data.
+parameter: 
+date : The last date of each month.  form date = "11-30-2021"
+"""
+
+
+def merge_v2(date):
+    base_CDC = "../data/CDC/"
+    base_JHU = "../data/JHU/"
+    df = pd.read_csv(base_CDC + "vaccinations-" + date + ".csv",
+                     converters={'FIPS': str})
+    deaths = pd.read_csv(
+        base_JHU + "deaths-05-01-2021-to-"+date+".csv", converters={'FIPS': str})
+
+    # Add the deaths data to the dataframe
+    return df.merge(deaths, on='FIPS')
+
+# test case 1
+# date = "11-30-2021"
+# df = merge(date)
+# print(df)
+
+
+"""This function write a csv to directory data/Merge. 
+parameter: 
+date : The last date of each month.  form date = "11-30-2021"
+"""
+def write_merge_data_to_csv(date):
+    base_Merge = "../data/Merge/"
+    df = merge_v1(date)
+    df.to_csv(base_Merge+"vaccinations-and-deaths-"+date+'.csv',index=False)
+
+
+# test case 2
+# date = "11-30-2021"
+# write_merge_data_to_csv(date)
+"""This function write 7 csvs to directory data/Merge. 
+
+"""
+
+def create_merge_data():
+    write_merge_data_to_csv("05-31-2021")
+    write_merge_data_to_csv("06-30-2021")
+    write_merge_data_to_csv("07-31-2021")
+    write_merge_data_to_csv("08-31-2021")
+    write_merge_data_to_csv("09-30-2021")
+    write_merge_data_to_csv("10-31-2021")
+    write_merge_data_to_csv("11-30-2021")
+    
