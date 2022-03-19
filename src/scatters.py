8f517a5d7597a326b704a364cfbce6f20d7101d6
MatthewjRay <ray.m@northeastern.edu>
2022-03-19 10:58:43 -0400

    Update scatters.py

--- src/scatters.py
+++ src/scatters.py
@@ -5,6 +5,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import sys
+import numpy as np
 
 def scatter(month):
     df = pd.read_csv('data/Merge/vaccinations-and-deaths-'+month+'.csv', converters={'FIPS' : str})
