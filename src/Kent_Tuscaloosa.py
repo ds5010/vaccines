195d11258e399c93c9e63519d2c40578c8361234
Philip <philip.eng.mathieu@gmail.com>
2022-03-27 22:30:18 -0400

    Refactored FIPS and created helper function fips_to_name

--- src/Kent_Tuscaloosa.py
+++ src/Kent_Tuscaloosa.py
@@ -1,8 +1,17 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+from collections import defaultdict
 
-def comparison():
+def fips_to_name(FIPS):
+    map = defaultdict(lambda: FIPS)
+    map.update({
+        '44003': 'Kent County, RI',
+        '01125': 'Tuscaloosa County, AL'
+    })
+    return map[FIPS]
+
+def comparison(FIPS_1='44003', FIPS_2='01125'):
     months = {
         'May':'05-31-2021',
         'June':'06-30-2021',
@@ -19,35 +28,40 @@ def comparison():
         df['Month'] = month
         file_list.append(df)
     merged_df = pd.concat(file_list)
-    cleaned_df = merged_df[merged_df['FIPS'].str.contains('44003|01125')]
+    cleaned_df = merged_df[merged_df['FIPS'].str.contains(FIPS_1+"|"+FIPS_2)]
 
-    Kent_vax = cleaned_df.loc[cleaned_df['FIPS']=='44003']
-    Kent_vax = Kent_vax["Series_Complete_18PlusPop_Pct"]
-    Tuscaloosa_vax = cleaned_df.loc[cleaned_df['FIPS']=='01125']
-    Tuscaloosa_vax = Tuscaloosa_vax["Series_Complete_18PlusPop_Pct"]
-    Kent_death = cleaned_df.loc[cleaned_df['FIPS']=='44003']
-    Kent_death = Kent_death['Deaths_Per_1e5']
-    Tuscaloosa_death = cleaned_df.loc[cleaned_df['FIPS']=='01125']
-    Tuscaloosa_death = Tuscaloosa_death['Deaths_Per_1e5']
+    FIPS_1_name = fips_to_name(FIPS_1)
+    FIPS_2_name = fips_to_name(FIPS_2)
+
+    FIPS_1_vax = cleaned_df.loc[cleaned_df['FIPS']==FIPS_1]
+    FIPS_1_vax = FIPS_1_vax["Series_Complete_18PlusPop_Pct"]
+    FIPS_1_death = cleaned_df.loc[cleaned_df['FIPS']==FIPS_1]
+    FIPS_1_death = FIPS_1_death['Deaths_Per_1e5']
+    
+    FIPS_2_vax = cleaned_df.loc[cleaned_df['FIPS']==FIPS_2]
+    FIPS_2_vax = FIPS_2_vax["Series_Complete_18PlusPop_Pct"]
+    FIPS_2_death = cleaned_df.loc[cleaned_df['FIPS']==FIPS_2]
+    FIPS_2_death = FIPS_2_death['Deaths_Per_1e5']
 
     fig,ax1 = plt.subplots() 
     x = [0, 1, 2, 3, 4, 5, 6]
     labels = ['May', 'June', 'July', 'August', 'September', 'October', 'November']
     x = np.arange(len(labels))
     width = 0.35 
-    plt.xticks(x, labels)
-    ax1.set_title('18+ Vaccination Rates and Deaths in Kent County, RI and Tuscaloosa County, AL', wrap=True)
+    ax1.set_xticks(x, labels)
+    ax1.set_ylim(0, 100)
+    ax1.set_title('18+ Vaccination Rates and Deaths in '+FIPS_1_name+" and "+FIPS_2_name, wrap=True)
     ax1.set_xlabel('Month 2021')
     ax1.set_ylabel('Percent Vaccinated') 
-    ax1.bar(x - width/2, Kent_vax, width, color='darkcyan', label = '% Vaxxed Kent')
-    ax1.bar(x + width/2, Tuscaloosa_vax, width, color='darkslategray', label = '% Vaxxed Tuscaloosa')
+    ax1.bar(x - width/2, FIPS_1_vax, width, color='darkcyan', label = '% Vaxxed in '+FIPS_1_name)
+    ax1.bar(x + width/2, FIPS_2_vax, width, color='darkslategray', label = '% Vaxxed in '+FIPS_2_name)
     ax2 = ax1.twinx()
-    ax2.plot(x, Kent_death, color='indianred', label = '# of Deaths Kent')
-    ax2.plot(x, Tuscaloosa_death, color='firebrick', label = '# of Deaths Tuscaloosa')
+    ax2.plot(x, FIPS_1_death, color='indianred', label = '# of Deaths in '+FIPS_1_name)
+    ax2.plot(x, FIPS_2_death, color='firebrick', label = '# of Deaths in '+FIPS_2_name)
     ax2.set_ylabel('Deaths per 1e5 of Pop.')
     handles_1, _ = ax1.get_legend_handles_labels()
     handles_2, _ = ax2.get_legend_handles_labels()
     handles_2.extend(handles_1) # adds the list items from handles_1 to handles_2
-    ax2.legend(handles = handles_2, loc='upper right')
-    plt.show()
+    ax2.legend(handles = handles_2, loc='upper left')
+    fig.savefig("img/comparison.png")
 comparison()
